eed6fc062e4da25bb6dd29f82880a17e8990d614
hoststreamsell <hoststreamsell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 16:38:11 +0000

    fixed php short open tag
    git-svn-id: https://plugins.svn.wordpress.org/hss-embed-streaming-video/trunk@1167841 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hss-embed-streaming-video.php
+++ hss-embed-streaming-video.php
@@ -6,7 +6,7 @@ Description: Provide access to Streaming Video in your WordPress Website
 Author: Gavin Byrne
 Author URI: https://www.hoststreamsell.com
 Contributors:
-Version: 0.6
+Version: 0.4
 
 HSS Embed Streaming Video is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -127,7 +127,7 @@ function hss_embed_options_page () {
                         </p>
                 </form>
         </div>
-<?
+<?php
 }
 
 function hss_embed_menu () {
